734ae83e56e3c9c947bbaa24d82964ce272e2edd
Maidele Archelus <maidele@usf.edu>
2025-01-26 20:38:24 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,7 @@
+// Task 1- Store Inventory
+
+let products = ["Computer", "Soap", "Body Wash", "Nail Polish", "Shoes"]; // Products within the array
+products.push("Flashlight");// Adds a new product
+products.pop();// Removes the product that is last within the array
+console.log("UpdatedProducts");
+
