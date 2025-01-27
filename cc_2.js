6c39f3a3a12b0a4a6080cbec3525b67b96ebddc8
Maidele Archelus <maidele@usf.edu>
2025-01-26 21:42:36 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -5,3 +5,11 @@ products.push("Flashlight");// Adds a new product
 products.pop();// Removes the product that is last within the array
 console.log("UpdatedProducts");
 
+//Task 2- Student Scores
+
+letscore = [100,200, 300, 400,500 ] // scores within the array
+scores[1] = 77; // Updating the 2nd score
+const averageScore = calculateAverage(scores);// Calculate the average score
+console.log ("Scores updated:, scores")
+console.log ("All score average:", average)
+
