fb8133911832068eb678ff4cd7114e7e3459ded1
Maidele Archelus <maidele@usf.edu>
2025-01-26 22:03:30 -0500

    Task 3 - Employee Records

--- cc_2.js
+++ cc_2.js
@@ -7,9 +7,21 @@ console.log("UpdatedProducts");
 
 //Task 2- Student Scores
 
-letscore = [100,200, 300, 400,500 ] // scores within the array
+letscore = [100,200, 300, 400,500 ]; // scores within the array
 scores[1] = 77; // Updating the 2nd score
 const averageScore = calculateAverage(scores);// Calculate the average score
-console.log ("Scores updated:, scores")
-console.log ("All score average:", average)
+console.log ("Scores updated:, scores");
+console.log ("All score average:", average);
 
+//Task 3-Employee Records
+let employee = { 
+    name: "Shelly Parks",
+    age: 22,
+    department: "Analytics",
+    isActive: true
+
+}; // array regarding the employee's properties
+
+employee.department = "Accounting"; // update property of department
+employee.position = "Vice president"; // includes a new property position
+console.log("Updatedemployee")
